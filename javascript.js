3430b05b0ea5679bf405abdba517a2c3948afb45
Stefanee <stefanee3866@gmail.com>
2022-06-20 10:57:24 -0600

    Fixed error in js file

--- javascript.js
+++ javascript.js
@@ -1,11 +1,11 @@
 function removeTransition(e) {
-    if (e.property !== 'transform') return;
+    if (e.propertyName !== 'transform') return;
     e.target.classList.remove('playing');
 }
 
 function playSound(e) {
-    const audio = document.querySelector('audio[data-key="${e.keyCode}"]');
-    const key = document.querySelector('div[data-key="${e.keyCode}"]');
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
 
     if (!audio) return;
 
